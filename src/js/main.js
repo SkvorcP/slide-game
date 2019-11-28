2dcaa27b54b336a7898a85b26fdb05d283aa9d4a
Peter <peter.skvorc@gmail.com>
2019-11-28 18:53:37 +0100

    Mouse Listener

--- src/js/main.js
+++ src/js/main.js
@@ -1,57 +1,112 @@
 // import { GamePanel } from '../../src/js/gamePanel.js';
 
 (function() {
-
-    var input = document.getElementById("n");
+    
+    var n;
+    var numPad;
+    var cell;
+    var gameEnd = false;
     var canvas = document.getElementById("myCanvas");
     var ctx = canvas.getContext("2d");
 
-    input.addEventListener('click', function(e) {
-        var n = eval(input.value);
-        let numTable = new GamePanel(n);
-        var numPad = numTable.resetGame();
-        ctx.clearRect(0, 0, canvas.width, canvas.height)
-        // numPadtoString(numPad, n);
-        drawPanel(numPad, n);
+    var input = document.getElementById("n");
+    input.addEventListener('click', function(e) {    
+        n = eval(input.value);
+        var numTable = new GamePanel(n);
+        numPad = numTable.resetGame();
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        cell = (canvas.width < canvas.height) ? canvas.width / n : canvas.height / n;   
+        drawPanel();
     });
 
+    canvas.addEventListener('click', function(e) {
+        moveCell(e);
+    });
+
+    function moveCell(e) {
+        var mouseY = e.clientX;
+        var mouseX = e.clientY;
+        var tx = Math.ceil(mouseX / cell);
+        var ty = Math.ceil(mouseY / cell);
+        var number = numPad[tx][ty];
+        console.log("Cell:" + cell);
+        console.log("X:" + mouseX);
+        console.log("Y:" + mouseY);
+        console.log(tx);
+        console.log(ty);
+        console.log("Number: " + number);
+
+        if ((tx <= n) && (ty <= n)) {
+
+            // up
+            if (number != 0 && numPad[tx + 1][ty] == 0) {
+                numPad[tx + 1][ty] = number;
+                numPad[tx][ty] = 0; 
+                drawPanel();
+            } 	
+            
+            // down
+            else if (number != 0 && numPad[tx - 1][ty] == 0) {
+                numPad[tx - 1][ty] = number;
+                numPad[tx][ty] = 0; 
+                drawPanel();
+            }	
+            
+            // left
+            else if (number != 0 && numPad[tx][ty + 1] == 0) {
+                numPad[tx][ty + 1] = number;
+                numPad[tx][ty] = 0; 
+                drawPanel();
+            }	
+            
+            // down
+            else if (number != 0 && numPad[tx][ty - 1] == 0) {
+                numPad[tx][ty - 1] = number;
+                numPad[tx][ty] = 0; 
+                drawPanel();
+            }
+        }
+    }
+        
     // ====================================
     // Draw numPad to Canvas
     // ====================================
-    function drawPanel(a, n) {
-        if (checkEnd(a, n)) {
-            alert("The End!");
-        }
-        else {
-            var cell = (canvas.width < canvas.height) ? canvas.width / n : canvas.height / n;   
-            var textSize = cell / 2;
-            var xStart = 0;
-            var yStart = 0;
-            for (var i = 0; i < n; i++) {
-                for (var j = 0; j < n; j++) {
-                    ctx.beginPath();
-                    ctx.rect(xStart, yStart, cell - 1, cell - 1);
-                    ctx.fillStyle = "#0095DD";
-                    ctx.fill();
-                    ctx.closePath();
-                    
-                    ctx.fillStyle = (a[i + 1][j + 1] == 0) ? 'purple' : 'yellow';
-                    ctx.font = textSize + 'px georgia';
-                    ctx.fillText(a[i + 1][j + 1], xStart + textSize / 2, yStart + textSize);
-                    xStart += cell + 1;
-                }
-                xStart = 0;
-                yStart += cell + 1;
+    function drawPanel() {
+
+        numPadtoString();
+        
+        var textSize = cell / 2;
+        var xStart = 0;
+        var yStart = 0;
+        for (var i = 0; i < n; i++) {
+            for (var j = 0; j < n; j++) {
+                ctx.beginPath();
+                ctx.rect(xStart, yStart, cell - 1, cell - 1);
+                ctx.fillStyle = "#0095DD";
+                ctx.fill();
+                ctx.closePath();
+                
+                ctx.fillStyle = (numPad[i + 1][j + 1] == 0) ? 'purple' : 'yellow';
+                ctx.font = textSize + 'px georgia';
+                ctx.fillText(numPad[i + 1][j + 1], xStart + textSize / 2, yStart + textSize);
+                xStart += cell + 1;
             }
+            xStart = 0;
+            yStart += cell + 1;
+        }
+        
+        checkEnd()
+        if (gameEnd) {
+            alert("The End!");
         }
     }
 
     // ====================================
     // Check, if the game is over
     // ====================================
-    function checkEnd(a, n) {
+    function checkEnd() {
         var z = 0;
-        var gameEnd = true;
+        gameEnd = true;
 		var zaporedneStevilke = [];
 		for(let i = 1; i < n * n; i++) {
 			zaporedneStevilke[i - 1] = i; 
@@ -59,24 +114,22 @@
 		}
 		for (let i = 1; i < n + 1; i++) {	
 			for (let j = 1; j < n + 1; j++) {
-				// console.log(a[j][i]);
-                if (a[j][i] != zaporedneStevilke[z])
+                if (numPad[i][j] != zaporedneStevilke[z])
                     gameEnd = false;
 					z++;
 			}
         }
-        return gameEnd;
     }
     
     // ====================================
     // NumPad to String
     // ====================================
-    function numPadtoString(a) {
+    function numPadtoString() {
 
         var vrstica = "";
-        for (let i = 0; i < a.length; i++) {
-            for (let j = 0; j < a.length; j++) {
-                vrstica += a[i][j] + "  ";
+        for (let i = 0; i < numPad.length; i++) {
+            for (let j = 0; j < numPad.length; j++) {
+                vrstica += numPad[i][j] + "  ";
             }
             console.log(vrstica + "\n");
             vrstica = "";
